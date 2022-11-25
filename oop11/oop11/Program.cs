67b6b1b3d9befb4580b902fc8a01a3ffcb4821c0
Anna <92574522+p1ndoska@users.noreply.github.com>
2022-11-25 23:55:37 +0300

    создание класса reflector содержащий методы исследования класса

--- oop11/oop11/Program.cs
+++ oop11/oop11/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Collections;
+
+namespace oop10
+{
+    class Program
+    {
+        public void Main()
+        {
+
+        }
+    }
+}
