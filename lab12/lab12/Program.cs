ed586ef9d680b93c2aff68f2af958dc5b75e48ed
Anna <92574522+p1ndoska@users.noreply.github.com>
2022-12-03 01:51:58 +0300

    create txt file

--- lab12/lab12/Program.cs
+++ lab12/lab12/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\2\oop\OOP\lab12\"
+            + "kaylogfile" + ".txt";
+
+            if (!File.Exists(path)) File.Create(path);
+
+        }
+    }
+}
