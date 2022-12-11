0ddc7bb2a9d6e260cb5f888a904eb4ea5fbf086f
Anna <92574522+p1ndoska@users.noreply.github.com>
2022-12-11 04:13:32 +0300

    finish KAYFileManager

--- lab12/lab12/KAYFileManager.cs
+++ lab12/lab12/KAYFileManager.cs
@@ -1,13 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 
-namespace lab12
+namespace Lab12
 {
-    internal class KAYFileManager
+    class KAYFileManager
     {
 
+        public static void Task1(string DISK, string path)
+        {
+
+
+            Directory.CreateDirectory(path); //создали директорий
+
+
+
+            DriveInfo drive = new DriveInfo(DISK);
+            var Files = drive.RootDirectory.GetFiles();
+            var Dir = drive.RootDirectory.GetDirectories();
+            string Newpath = Path.Combine(path, @"\KAYdirinfo.txt");
+            using (StreamWriter file = new StreamWriter(Newpath))
+            {
+                file.WriteLine("______________________________________________");
+                file.WriteLine("Список дирректориев");
+
+                foreach (var i in Dir)
+                {
+                    file.WriteLine(i);
+                }
+                file.WriteLine("______________________________________________");
+                file.WriteLine("Список файлов");
+                foreach (var i in Files)
+                {
+                    file.WriteLine(i);
+                }
+            }
+            FileInfo filenew = new FileInfo(Newpath);
+            if (!File.Exists(path + @"D:\2\oop\OOP\lab12\KAYdirinfoCOPY.txt"))
+            {
+                filenew.CopyTo(path + @"\KAYdirinfoCOPY.txt");
+            }
+            filenew.Delete();
+        }
+
+        public static void Task2(string path)
+        {
+            Directory.CreateDirectory(path); //создали директорий
+            DirectoryInfo buff = new DirectoryInfo(@"D:\2\oop\OOP\lab12\KAYbuff");
+            var files = buff.GetFiles();
+            foreach (var f in files)
+            {
+
+                if (f.Extension == ".TXT" && !File.Exists(path + $@"\{f.Name}"))
+                {
+
+                    File.Copy(f.FullName, path + $@"\{f.Name}");
+                }
+            }
+            if (!Directory.Exists(@"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles") && Directory.Exists(path))
+            {
+                Directory.Move(path, @"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles");
+            }
+        }
+
+        public static void Task3()
+        {
+            if (!File.Exists(@"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles.zip"))
+            {
+                ZipFile.CreateFromDirectory(@"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles", @"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles.zip");
+            }
+            if (!Directory.Exists(@"D:\2\oop\OOP\lab12\KAYInspect\BASFilesEXTR"))
+            {
+                ZipFile.ExtractToDirectory(@"D:\2\oop\OOP\lab12\KAYInspect\KAYFiles.zip", @"D:\2\oop\OOP\lab12\KAYInspect\BASFilesEXTR");
+            }
+        }
     }
+
 }
+
