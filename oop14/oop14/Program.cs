d433dad078b34a96aa99ea4cf62db95a8752fe71
Anna <92574522+p1ndoska@users.noreply.github.com>
2022-12-13 05:51:48 +0300

    name configuration detailes all assemblys

--- oop14/oop14/Program.cs
+++ oop14/oop14/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First();
+            //Second();///////////
+            //Third();
+
+            //Fourth();
+            //Fifth();
+        }
+
+
+        private static void First()
+        {
+            var allProcesses = Process.GetProcesses();
+            Console.WriteLine("Информация о процессах:");
+            Console.Write("{0,-10}", "ID:");
+            Console.Write("{0,-70}", "Process Name:");
+            Console.Write("{0,-20}", "Priority:");
+            Console.Write("{0,-10}", "Memory:\n");
+
+            foreach (Process process in allProcesses)
+            {
+                Console.Write("{0,-10}", $"{process.Id}");
+                Console.Write("{0,-70}", $"{process.ProcessName}");
+                Console.Write("{0,-20}", $"{process.BasePriority}");
+                Console.Write("{0,-10}", $"{process.WorkingSet64}");
+
+                Console.WriteLine();
+            }
+
+        }
+        private static void Second()
+        {
+
+            AppDomain domain = AppDomain.CurrentDomain;    // текущий домен с процессами
+            Console.WriteLine("\n\n\n\nТекущий домен:         " + domain.FriendlyName);
+            Console.WriteLine("Базовый каталог:       " + domain.BaseDirectory);
+            Console.WriteLine("Детали конфигурации:   " + domain.SetupInformation);
+            Console.WriteLine("Все сборки в домене:\n");
+            foreach (Assembly ass in domain.GetAssemblies())
+                Console.WriteLine(ass.GetName().Name);
+
+        }
+
+    }
+}
